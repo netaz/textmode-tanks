b8d18776b1e93f19c001fb300520e534c7b92f6d
Neta Zmora <neta.zmora@gmail.com>
2015-10-03 23:43:43 +0300

    Roii's first commit!
    1. Create a 2D map
    2. Create an key-press input loop which accepts arrow- keys presses to
    move the "tank" (currently only a cursor) and "Q" to quit
    3. Move the "tank" around the map, while checking for boundaries
    4. When quitting the game: clear the screen and display a special GG for
    2 seconds before quitting

---
+++ textmode-tanks/Program.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace textmode_tanks
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Tank War";
+            Console.BackgroundColor = ConsoleColor.DarkRed;
+            Console.WriteLine("#############");
+            Console.WriteLine("#   #   # # #");
+            Console.WriteLine("# #   #   # #");
+            Console.WriteLine("# #####  #  #");
+            Console.WriteLine("#        #  #");
+            Console.WriteLine("######## #  #");
+            Console.WriteLine("#   #  # #  #");
+            Console.WriteLine("# # #  #  # #");
+            Console.WriteLine("# # #  #  # #");
+            Console.WriteLine("# #    #    #");
+            Console.WriteLine("# #    #  # #");
+            Console.WriteLine("# ###  ##  ##");
+            Console.WriteLine("#   #  ##   #");
+            Console.WriteLine("#      #  # #");
+            Console.WriteLine("##  #       #");
+            Console.WriteLine("#############");
+            Console.SetCursorPosition(1, 1);
+            while (true)
+            {
+                ConsoleKeyInfo keyinfo = Console.ReadKey(true);
+                if (keyinfo.Key == ConsoleKey.RightArrow)
+                {
+                    if (Console.CursorLeft < 12)
+                    {
+                        Console.CursorLeft = Console.CursorLeft + 1;
+                    }
+                }
+
+                if (keyinfo.Key == ConsoleKey.LeftArrow)
+                {
+                    if (Console.CursorLeft > 0)
+                    {
+                        Console.CursorLeft = Console.CursorLeft - 1;
+                    }
+                }
+
+                if (keyinfo.Key == ConsoleKey.UpArrow)
+                {
+                    if (Console.CursorTop > 0)
+                    {
+                        Console.CursorTop = Console.CursorTop - 1;
+                    }
+                }
+
+                if (keyinfo.Key == ConsoleKey.DownArrow)
+                {
+                    if (Console.CursorTop < 15)
+                    {
+                        Console.CursorTop = Console.CursorTop + 1;
+                    }
+                }
+
+                if (keyinfo.Key == ConsoleKey.Q)
+                {
+                    // exit the application
+                    break;
+                }
+
+                
+            }
+
+            Console.Clear();
+            string gg = " _____ _____\n" +
+                        "|  __ \\  __ \\\n" +
+                        "| |  \\/ |  \\/\n" +
+                        "| | __| | __ \n" +
+                        "| |_\\ \\ |_\\ \\\n" +
+                        "\\____/\\____/\n";
+
+            Console.WriteLine(gg);
+            System.Threading.Thread.Sleep(2000);
+            
+        }
+    }
+}
+
+
+        

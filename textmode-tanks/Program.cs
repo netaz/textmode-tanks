346d3e55c38288f822d3ba742bf164ac04efa6bf
Neta Zmora <neta.zmora@gmail.com>
2015-10-04 23:55:14 +0300

    This commit is all about wall detection.
    Fortunately, Roii understands the Cartesian coordinates system so he
    quickly understood why we need to keep track of the tank's (x,y)
    coordinates.  As a preliminary step, we converted the series of
    Console.WriteLine calls to a single map string.
    Whenever the tank makes a move, we refer back to this map, using the
    tank's coordinates and check if the "future" move places the tank on the
    road (represented by spaces), or on a wall (represented by the pound
    sign).
    Converting the 2-dimensional (x,y) coordinates to a 1-dimensional
    coordinate on the 'map' string took several explanations.  He seemed to
    understand the concept, but it is far from trivial.  I also explained a
    bit about the memory representation of strings.
    As usual, I worked with him, showing him how to write the code for
    wall-detection when moving right and he completed the code for the other
    three directions.
    We also added a display of the tank's (x,y) coordinates at the bottom of
    the screen, which he thought was cool.  As before, we worked out the
    solution while discussing possible solutions, experimenting, fixing and
    reiterating.

--- textmode-tanks/Program.cs
+++ textmode-tanks/Program.cs
@@ -10,25 +10,30 @@ namespace textmode_tanks
     {
         static void Main(string[] args)
         {
+            int x = 1, y = 1;
             Console.Title = "Tank War";
             Console.BackgroundColor = ConsoleColor.DarkRed;
-            Console.WriteLine("#############");
-            Console.WriteLine("#   #   # # #");
-            Console.WriteLine("# #   #   # #");
-            Console.WriteLine("# #####  #  #");
-            Console.WriteLine("#        #  #");
-            Console.WriteLine("######## #  #");
-            Console.WriteLine("#   #  # #  #");
-            Console.WriteLine("# # #  #  # #");
-            Console.WriteLine("# # #  #  # #");
-            Console.WriteLine("# #    #    #");
-            Console.WriteLine("# #    #  # #");
-            Console.WriteLine("# ###  ##  ##");
-            Console.WriteLine("#   #  ##   #");
-            Console.WriteLine("#      #  # #");
-            Console.WriteLine("##  #       #");
-            Console.WriteLine("#############");
-            Console.SetCursorPosition(1, 1);
+            string map =    "#############\n" +
+                            "#   #   # # #\n" +
+                            "# #   #   # #\n" +
+                            "# #   #   # #\n" +
+                            "# #####  #  #\n" +
+                            "#        #  #\n" +
+                            "######## #  #\n" +
+                            "#   #  # #  #\n" +
+                            "# # #  #  # #\n" +
+                            "# # #  #  # #\n" +
+                            "# #    #    #\n" +
+                            "# #    #  # #\n" +
+                            "# ###  ##  ##\n" +
+                            "#   #  ##   #\n" +
+                            "#      #  # #\n" +
+                            "##  #       #\n" +
+                            "#############";
+            Console.WriteLine(map);
+            Console.WriteLine("(x,y) = (" + x + "," + y + ")");
+            Console.SetCursorPosition(x, y);
+            Console.CursorSize = 100;
             while (true)
             {
                 ConsoleKeyInfo keyinfo = Console.ReadKey(true);
@@ -36,7 +41,19 @@ namespace textmode_tanks
                 {
                     if (Console.CursorLeft < 12)
                     {
-                        Console.CursorLeft = Console.CursorLeft + 1;
+
+                        int characters_to_skip = y * 14 + (x + 1);
+                        if (map[characters_to_skip] == ' ') // check if there is a space to the right 
+                        {
+                            // no wall, so we can walk right
+                            x = x + 1;
+
+                        }
+
+                    }
+                    else
+                    {
+                        Console.Beep();
                     }
                 }
 
@@ -44,7 +61,15 @@ namespace textmode_tanks
                 {
                     if (Console.CursorLeft > 0)
                     {
-                        Console.CursorLeft = Console.CursorLeft - 1;
+                        int characters_to_skip = y * 14 + (x - 1);
+                        if (map[characters_to_skip] == ' ')
+                        {
+                            x = x - 1;
+                        }
+                    }
+                    else
+                    {
+                        Console.Beep();
                     }
                 }
 
@@ -52,7 +77,15 @@ namespace textmode_tanks
                 {
                     if (Console.CursorTop > 0)
                     {
-                        Console.CursorTop = Console.CursorTop - 1;
+                        int characters_to_skip = (y - 1) * 14 + x;
+                        if (map[characters_to_skip] == ' ')
+                        {
+                            y = y - 1;
+                        }
+                    }
+                    else
+                    {
+                        Console.Beep();
                     }
                 }
 
@@ -60,7 +93,15 @@ namespace textmode_tanks
                 {
                     if (Console.CursorTop < 15)
                     {
-                        Console.CursorTop = Console.CursorTop + 1;
+                        int characters_to_skip = (y + 1) * 14 + x;
+                        if (map[characters_to_skip] == ' ')
+                        {
+                            y = y + 1;
+                        }
+                    }
+                    else
+                    {
+                        Console.Beep();
                     }
                 }
 
@@ -70,7 +111,11 @@ namespace textmode_tanks
                     break;
                 }
 
-                
+                Console.CursorTop = 17;
+                Console.CursorLeft = 0;
+                Console.WriteLine("(x,y) = (" + x + "," + y + ")");
+                Console.CursorTop = y;
+                Console.CursorLeft = x;
             }
 
             Console.Clear();
@@ -83,7 +128,7 @@ namespace textmode_tanks
 
             Console.WriteLine(gg);
             System.Threading.Thread.Sleep(2000);
-            
+
         }
     }
 }
